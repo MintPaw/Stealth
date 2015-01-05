1109a9011d0a6efdd71a3331feaa780c05bf66c0
MintPaw <jerusanders@gmail.com>
2015-01-05 01:03:14 -0700

    Added FlxMintInput

--- Source/Main.hx
+++ Source/Main.hx
@@ -2,8 +2,10 @@ package;
 
 
 import flixel.FlxGame;
+import flxMintInput.FlxMintInput;
 import game.GameState;
 import openfl.display.Sprite;
+import openfl.events.Event;
 
 
 class Main extends Sprite
@@ -11,9 +13,17 @@ class Main extends Sprite
 
 	public function new()
 	{
+		addEventListener(Event.ADDED_TO_STAGE, init);
+		super();
+	}
+	
+	private function init(e:Event):Void
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, init);
+		
+		FlxMintInput.init(stage);
+		
 		var flixel:FlxGame = new FlxGame(1280, 720, GameState, 1, 60, 60, true);
 		addChild(flixel);
-
-		super();
 	}
 }
