fb8a92081a5440f2e2e12a632e5cf0ea2b1a4e4a
MintPaw <jerusanders@gmail.com>
2015-01-15 17:32:13 -0700

    Set up the basic state machine documentation and added some more future use variables

--- Source/game/Enemy.hx
+++ Source/game/Enemy.hx
@@ -12,7 +12,7 @@ class Enemy extends FlxSprite
 	public static var SHOOTING:Int = 2;
 	public static var GUESS_SHOOTING:Int = 3;
 	public static var CHASING:Int = 4;
-	public static var MOVE_BACK:Int = 5;
+	public static var MOVING_BACK:Int = 5;
 	
 	public var gun:FlxSprite;
 	
@@ -22,6 +22,9 @@ class Enemy extends FlxSprite
 	private var _idle:Bool = true;
 	private var _state:Int = 0;
 	private var _stateMachineDocs:Map<Int, Array<Int>>;
+
+	private var _player:Player;
+	private var _lastSeenPlayer:FlxPoint;
 	
 	public function new()
 	{
@@ -35,16 +38,25 @@ class Enemy extends FlxSprite
 		
 		buildStateMachineDocs();
 	}
-	
-	
-	private function buildStateMachineDocs():Void
-	{
-		_stateMachineDocs = new Map();
-	}
-	
+
 	public function seePlayer(p:Player):Void
 	{
 		
+	}
+
+	private function buildStateMachineDocs():Void
+	{
+		_stateMachineDocs = new Map();
+		_stateMachineDocs.set(IDLE, [SHOOTING, CHASING]);
+		_stateMachineDocs.set(SHOOTING, [GUESS_SHOOTING, CHASING]);
+		_stateMachineDocs.set(GUESS_SHOOTING, [MOVING_BACK]);
+		_stateMachineDocs.set(CHASING, [SHOOTING, MOVING_BACK]);
+	}
+	
+
+	private function switchState(s:Int):Bool
+	{
+
 	}
 	
 	override public function update(elapsed:Float):Void 
