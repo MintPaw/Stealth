59bc27c480ddf7e211cc2678e48e83cf468f5b59
MintPaw <jerusanders@gmail.com>
2015-02-01 17:44:23 -0700

    Added sound playing system and embeded one effect

--- Source/Sm.hx
+++ Source/Sm.hx
@@ -1,4 +1,6 @@
 package ;
+import openfl.Assets;
+import openfl.media.SoundChannel;
 
 /**
  * ...
@@ -6,10 +8,18 @@ package ;
  */
 class Sm
 {
-
+	public static inline var ENEMY_SHOOT:String = "Assets/sound/enemyShoot.wav";
+	
+	private static var _effectChannel:SoundChannel;
+	
 	public function new() 
 	{
 		
+	}
+	
+	public static function playEffect(effectName:String):Void
+	{
+		_effectChannel = Assets.getSound(effectName).play(0, 0);
 	}
 	
 }
